85d6feaf9ddcd7451f20100382a6ae07a32ef7ad
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-08-21 00:45:20 +0300

    a

--- components/mesajlar/all-messages.tsx
+++ components/mesajlar/all-messages.tsx
@@ -6,39 +6,9 @@ import { Button } from '../ui/button'
 import Link from 'next/link'
 const AllMessages = async () => {
 
-    const categories = await prismadb.category.findMany(
-        {
-            include: {   // fetch products data with categories
-                items: true
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        }
-    )
-    console.log("categories:", categories)
     return (
         <div>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>İsim</TableHead>
-                        <TableHead>İsim</TableHead>
-                        <TableHead>İsim</TableHead>
-                        <TableHead>İsim</TableHead>
-                        <TableHead>İsim</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>Sa</TableCell>
-                        <TableCell>Sa</TableCell>
-                        <TableCell>Sa</TableCell>
-                        <TableCell>Sa</TableCell>
-                        <TableCell>Sa</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+
         </div>
     )
 }
