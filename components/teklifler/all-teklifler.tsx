0d220b846b175c44cc78962a02b2f8c5f3348d89
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-08-26 22:06:53 +0300

    apı

--- components/teklifler/all-teklifler.tsx
+++ components/teklifler/all-teklifler.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import prismadb from '@/lib/prismadb'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
-import CategoryActions from './category-actions'
 import { Button } from '../ui/button'
 import Link from 'next/link'
 const AllTeklifler = async () => {
