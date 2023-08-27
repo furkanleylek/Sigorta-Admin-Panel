a7f267df97e834ae26a61f2590db271e4cccdad0
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-08-27 21:29:17 +0300

    force dynamic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import AllCards from '@/components/dashboard/all-cards'
+
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return (
     <main className="flex  flex-col items-center justify-between p-24">
