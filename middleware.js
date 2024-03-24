adeadfd621f60ee95de45e374aa1538af8ccd157
Furkan Leylek <furkanyasinn.1@outlook.com>
2024-03-24 20:16:46 +0300

    ss

--- middleware.js
+++ middleware.js
@@ -1,48 +1,48 @@
-import { NextResponse } from 'next/server'
-import isAuthPages from './utils/isAuthPages'
-export async function middleware(request) {
+// import { NextResponse } from 'next/server'
+// import isAuthPages from './utils/isAuthPages'
+// export async function middleware(request) {
 
-    const { url, nextUrl, cookies } = request
-    const { value: token } = cookies.get('token') ?? { value: null } // to name of value with token
-    const isAuthPageRequested = isAuthPages(nextUrl.pathname)
+//     const { url, nextUrl, cookies } = request
+//     const { value: token } = cookies.get('token') ?? { value: null } // to name of value with token
+//     const isAuthPageRequested = isAuthPages(nextUrl.pathname)
 
-    if (isAuthPageRequested) {
-        if (!token) {
-            const response = NextResponse.next() // if user want to go auth pages(login is now) but not have token
-            return response
-        }
-        const response = NextResponse.redirect(new URL(`/`, url)); // if user want to go to the login page but user have token
-        return response
-    }
+//     if (isAuthPageRequested) {
+//         if (!token) {
+//             const response = NextResponse.next() // if user want to go auth pages(login is now) but not have token
+//             return response
+//         }
+//         const response = NextResponse.redirect(new URL(`/`, url)); // if user want to go to the login page but user have token
+//         return response
+//     }
 
-    // if user dont have token , route the login page
-    if (!token) {
+//     // if user dont have token , route the login page
+//     if (!token) {
 
-        // kind of keeping the destination in memory, redirecting there after login
-        const searchParams = new URLSearchParams(nextUrl.searchParams)
-        searchParams.set("next", nextUrl.pathname)
+//         // kind of keeping the destination in memory, redirecting there after login
+//         const searchParams = new URLSearchParams(nextUrl.searchParams)
+//         searchParams.set("next", nextUrl.pathname)
 
-        const response = NextResponse.redirect(
-            new URL(`/login?${searchParams}`, url)
-        );
-        response.cookies.delete("token");
-        return response;
-    }
+//         const response = NextResponse.redirect(
+//             new URL(`/login?${searchParams}`, url)
+//         );
+//         response.cookies.delete("token");
+//         return response;
+//     }
 
-    return NextResponse.next()
-}
+//     return NextResponse.next()
+// }
 
-export const config = {
-    matcher: [
-        '/',
-        '/trafik',
-        '/kasko',
-        '/konut',
-        '/isyeri',
-        '/dask',
-        '/ferdikaza',
-        '/digerkategoriler',
-        '/mesajlar',
-        '/login'
-    ]
-}
+// export const config = {
+//     matcher: [
+//         '/',
+//         '/trafik',
+//         '/kasko',
+//         '/konut',
+//         '/isyeri',
+//         '/dask',
+//         '/ferdikaza',
+//         '/digerkategoriler',
+//         '/mesajlar',
+//         '/login'
+//     ]
+// }
